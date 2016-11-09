9b877a90b7149e1a996ad020b8bd6a12e3c94fa1
jonathan <jony327@gmail.com>
2016-11-08 22:36:59 -0500

    fix

--- apps/store/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import ReferenceGuideTallerOrder, ReferenceGuideTallerOrderDetail
-
-admin.site.register(ReferenceGuideTallerOrder)
-admin.site.register(ReferenceGuideTallerOrderDetail)
